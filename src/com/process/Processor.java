d68503ac61d0689c18bb3f4159c378f10332a24b
Alvaro <alvpcardoso@gmail.com>
2022-03-04 10:44:48 +0100

    002: Updated Menu and Summary sorting.

--- src/com/process/Processor.java
+++ src/com/process/Processor.java
@@ -3,13 +3,11 @@ package com.process;
 import com.obj.Match;
 import com.obj.TeamScore;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Scanner;
+import java.util.*;
 
 public class Processor {
 
-    Collection<Match> matches;
+    List<Match> matches;
     Match currentMatch;
     int matchCode = 0;
     boolean matchStatus = false;
@@ -20,25 +18,31 @@ public class Processor {
 
         String homeTeam;
         String awayTeam;
-        int choice;
+        String choice;
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
         while(true) {
 
             //create menu
+            System.out.println("\n");
             System.out.println("### World Cup Score ###");
-            System.out.println("Press 1 to start a game");
-            System.out.println("Press 2 to finish a game");
-            System.out.println("Press 3 to update a game score");
-            System.out.println("Press 4 to get a summary");
-            System.out.println("Press 5 to quit\n \n ");
+            System.out.println("Press 1 to start a game.");
+            System.out.println("Press 2 to finish current game.");
+            System.out.println("Press 3 to update current game score.");
+            System.out.println("Press 4 to get a summary.");
+            System.out.println("Press 5 to quit \n");
 
             System.out.println("Select your option:");
-            choice = scanner.nextInt();
+            try{
+                choice = scanner.next();
+            } catch (InputMismatchException e){
+                System.out.println("Invalid option. Select an option from 1 to 5. \n");
+                continue;
+            }
 
             switch (choice) {
 
                 // Start a game
-                case 1:
+                case "1":
 
                     if(matchCode == 5){
                         System.out.println("You can't add more matches. \n");
@@ -57,7 +61,7 @@ public class Processor {
                     break;
 
                 // Finish a game
-                case 2:
+                case "2":
                     if(!matchStatus){
                         System.out.println("First start a game. \n");
                     }
@@ -67,7 +71,7 @@ public class Processor {
                     break;
 
                 // Update score
-                case 3:
+                case "3":
 
                     if(!matchStatus){
                         System.out.println("First start a game. \n");
@@ -84,13 +88,14 @@ public class Processor {
                     break;
 
                 // Summary
-                case 4:
+                case "4":
 
                     if(!matches.isEmpty()){
                         System.out.println("Cup summary \n");
+                        // sort matches by score
+                        sortMatchesByScore();
                         for(Match match : matches){
                             showCurrentMatch(match);
-                            System.out.println("\n");
                         }
                     }
                     else{
@@ -100,11 +105,11 @@ public class Processor {
                     break;
 
                 // Quit
-                case 5:
+                case "5":
                     System.exit(0);
                     // invalid option
                 default:
-                    System.out.println("Invalid option. Select an option from 1 to 5. \\n\\n");
+                    System.out.println("Invalid option. Select an option from 1 to 5. \n");
             }
         }
 
@@ -125,8 +130,8 @@ public class Processor {
     }
 
     public void updateMatch(int homeScore, int awayScore){
-        currentMatch.getAwayTeam().setScore(homeScore);
-        currentMatch.getHomeTeam().setScore(awayScore);
+        currentMatch.getHomeTeam().setScore(homeScore);
+        currentMatch.getAwayTeam().setScore(awayScore);
     }
 
     public void finishMatch(){
@@ -138,6 +143,25 @@ public class Processor {
     public void showCurrentMatch(Match currentMatch){
         System.out.println(currentMatch.getHomeTeam().getTeamName() + " " + currentMatch.getHomeTeam().getScore()  + " - " +
                 currentMatch.getAwayTeam().getTeamName() + " " + currentMatch.getAwayTeam().getScore());
+    }
+
+    public void sortMatchesByScore(){
+        Collections.sort(matches, new Comparator<>() {
+
+            @Override
+            public int compare(Match o1, Match o2) {
+                int match1HomeScore = o1.getHomeTeam().getScore();
+                int match1AwayScore = o1.getAwayTeam().getScore();
+                int totalMatch1 = match1HomeScore + match1AwayScore;
+
+                int match2HomeScore = o2.getHomeTeam().getScore();
+                int match2AwayScore = o2.getAwayTeam().getScore();
+                int totalMatch2 = match2HomeScore + match2AwayScore;
+
+                return totalMatch2 - totalMatch1;
+            }
+
+        });
     }
 
 }
